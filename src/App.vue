f3df863f779975ee1ed8322bdb12c64b7fec3a66
Mohammed <mohammed@macbook-air-de-mohammed.home>
2020-12-20 00:18:50 +0100

    init vuejs project

---
+++ src/App.vue
@@ -0,0 +1,199 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-icon>mdi-star-circle</v-icon>
+      <div class="ml-2">
+        <h2>Skills Banner Generator</h2>
+      </div>
+      <v-spacer></v-spacer>
+      <a href='https://github.com/boukadam/skills-banner-generator'><v-icon large>mdi-github</v-icon></a>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-card class="mx-lg-auto mt-6 pa-2" :width="canvasWidth + 10">
+          <v-card-title>
+            <h2>Please select your skills</h2>
+          </v-card-title>
+          <v-autocomplete
+            v-model="selected"
+            :items="skills"
+            :search-input.sync="search"
+            label="Your skills"
+            multiple
+            clearable
+            chips
+            hide-selected
+            item-value="files[0]"
+            item-text="name"
+          >
+            <template v-slot:item="data">
+              <v-row class="ml-2">
+                <img
+                  :src="require('./static/logos/' + data.item.files[0])"
+                  width="24"
+                />
+                <span class="pl-2">{{ data.item.name }}</span>
+              </v-row>
+            </template>
+            <template v-slot:selection="data">
+              <v-chip close @click:close="onSkillRemoved(data)">
+                <span>{{ data.item.name }}</span>
+              </v-chip>
+            </template>
+          </v-autocomplete>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="generate"> Generate </v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card class="mx-lg-auto mt-6" :width="canvasWidth + 10">
+          <canvas id="myCanvas" :width="canvasWidth" :height="canvasHeight">
+            Your browser does not support the HTML5 canvas tag.
+          </canvas>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="download">
+              Download as PNG
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import json from "./static/logos.json";
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    skills: json,
+    selected: [],
+    search: null,
+    canvasWidth: 800,
+    canvasHeight: 200,
+    logoSize: 40,
+    logoMargin: 4,
+  }),
+  computed: {
+    logoArea() {
+      return this.logoSize + 2 * this.logoMargin;
+    },
+    nbLogoPerRow() {
+      return Math.floor(this.canvasWidth / this.logoArea);
+    },
+    nbLogoPerColumn() {
+      return Math.floor(this.canvasHeight / this.logoArea);
+    },
+  },
+  watch: {
+    selected() {
+      this.search = null;
+    },
+  },
+  mounted() {
+    this.drawCanvasBackground();
+  },
+  methods: {
+    drawCanvasBackground() {
+      var canvas = document.getElementById("myCanvas");
+      var ctx = canvas.getContext("2d");
+      // Create gradient
+      var grd = ctx.createRadialGradient(
+        0,
+        this.canvasWidth / 4,
+        5,
+        0,
+        this.canvasWidth / 4,
+        this.canvasHeight / 2
+      );
+      grd.addColorStop(1, "whitesmoke");
+      grd.addColorStop(0, "white");
+      // Fill with gradient
+      ctx.fillStyle = grd;
+      ctx.fillRect(10, 10, this.canvasWidth, this.canvasHeight);
+    },
+    onSkillRemoved(data) {
+      data.parent.selectItem(data.item);
+    },
+    computeCoordinate(index) {
+      var x = (index % this.nbLogoPerRow) + 1;
+      var y = Math.floor(index / this.nbLogoPerRow) + 1;
+      return {
+        x: this.canvasWidth - x * this.logoArea,
+        y: this.canvasHeight - y * this.logoArea,
+      };
+    },
+    generate() {
+      var c = document.getElementById("myCanvas");
+      var ctx = c.getContext("2d");
+      var _this = this;
+      this.selected.forEach((element, index) => {
+        var image = new Image();
+        image.addEventListener("load", function () {
+          var height = _this.logoSize;
+          var width = _this.logoSize * (this.naturalWidth / this.naturalHeight);
+          if (width > _this.logoSize) {
+            width = _this.logoSize;
+            height = (_this.logoSize * this.naturalHeight) / this.naturalWidth;
+          }
+          var coordinate = _this.computeCoordinate(index);
+          var yShift = 0;
+          if (height < _this.logoSize) {
+            yShift = (_this.logoSize - height) / 2;
+          }
+          ctx.drawImage(
+            image,
+            coordinate.x,
+            coordinate.y + yShift,
+            width,
+            height
+          );
+        });
+        image.src = require("./static/logos/" + element);
+      });
+    },
+    download() {
+      var canvas = document.getElementById("myCanvas");
+      var lnk = document.createElement("a"),
+        e;
+
+      /// the key here is to set the download attribute of the a tag
+      lnk.download = "skills-banner.png";
+
+      /// convert canvas content to data-uri for link. When download
+      /// attribute is set the content pointed to by link will be
+      /// pushed as "download" in HTML5 capable browsers
+      lnk.href = canvas.toDataURL("image/png;base64");
+
+      /// create a "fake" click-event to trigger the download
+      if (document.createEvent) {
+        e = document.createEvent("MouseEvents");
+        e.initMouseEvent(
+          "click",
+          true,
+          true,
+          window,
+          0,
+          0,
+          0,
+          0,
+          0,
+          false,
+          false,
+          false,
+          false,
+          0,
+          null
+        );
+
+        lnk.dispatchEvent(e);
+      } else if (lnk.fireEvent) {
+        lnk.fireEvent("onclick");
+      }
+    },
+  },
+};
+</script>
