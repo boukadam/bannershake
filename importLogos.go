1a973159bd211e0a75a2397c7070f1f9de332bd8
Mohammed BOUKADA <m.boukada@gmail.com>
2021-05-20 21:59:09 +0200

    update README

--- importLogos.go
+++ importLogos.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/otiai10/copy"
+	"io"
 	"io/ioutil"
 	"os"
 	"strings"
+	"path"
 )
 
 type BannerShakeLogos struct {
@@ -85,7 +86,7 @@ func main() {
 
 	_ = os.Mkdir(providedLogosBase, 0755)
 	_ = ioutil.WriteFile(providedLogosBase + "providedLogos.json", jsonOutput, 0644)
-	_ = copy.Copy(logosRepo + "/logos", providedLogosDir)
+	_ = CopyDir(logosRepo + "/logos", providedLogosDir)
 
 	for _, logoToRemove := range logosToRemove {
 		_ = os.Remove(providedLogosDir + logoToRemove)
@@ -95,4 +96,62 @@ func main() {
 		_ = os.Rename(providedLogosDir + logoToRename, providedLogosDir + logosBlockedByAdblock[logoToRename])
 	}
 
+}
+
+func CopyDir(src string, dst string) error {
+	var err error
+	var fds []os.FileInfo
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcfp := path.Join(src, fd.Name())
+		dstfp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = CopyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = CopyFile(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
+}
+
+func CopyFile(src, dst string) error {
+	var err error
+	var srcfd *os.File
+	var dstfd *os.File
+	var srcinfo os.FileInfo
+
+	if srcfd, err = os.Open(src); err != nil {
+		return err
+	}
+	defer srcfd.Close()
+
+	if dstfd, err = os.Create(dst); err != nil {
+		return err
+	}
+	defer dstfd.Close()
+
+	if _, err = io.Copy(dstfd, srcfd); err != nil {
+		return err
+	}
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	return os.Chmod(dst, srcinfo.Mode())
 }
