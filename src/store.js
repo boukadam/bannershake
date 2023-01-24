1bb6c9a412e51164ecc282d3980d9656f24548c9
Mohammed BOUKADA <m.boukada@gmail.com>
2023-01-24 18:24:53 +0100

    add managed logos, add gatling and karate logos

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
+import _ from 'lodash'
 
 import Vuex from "vuex";
-import json from "@/static/provided/providedLogos.json";
+import managedLogos from "@/static/managed/managedLogos.json";
+import providedLogos from "@/static/provided/providedLogos.json";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        skills: json
+        skills: _.sortBy(_.unionBy(managedLogos, providedLogos, 'shortname'), 'shortname')
     }
 })
